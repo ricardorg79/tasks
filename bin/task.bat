2b1687ad1e7294e8909fc73339d2001316de3d35
Ricardo Ramirez <ricardo@rhamerica.com>
2024-04-04 09:17:06 -0700

    testing

---
+++ bin/task.bat
@@ -0,0 +1,5 @@
+@echo off
+setlocal
+
+%BAZURTO_PYTHON_BINDIR%\python %TASKPROJECTDIR%\bin\task.py "$@"
+
--- src/main.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-
-%BAZURTO_PYTHON_BINDIR%\python $DIR\src\main.py "$@"
-
